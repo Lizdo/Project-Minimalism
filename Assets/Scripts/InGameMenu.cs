48bf633b362268b2431f165c80e4ca4186688b2f
lizdo <lizdourden@gmail.com>
2013-05-12 11:22:51 +0800

    Round Label

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -32,24 +32,39 @@ public class InGameMenu : MonoBehaviour {
 				gameLogic.AddPlayerUnit();
 			}	
 		}else{
-			SetDisabledColor();
+			GUI.color = disabledColor;
 			GUI.Button (new Rect (10,40,80,80), new GUIContent ("Add Unit\n(Cost 50)", icons[iconNames[0]]));
-			RestoreDisabledColor();
+			RestoreGUIColor();
 
 		}
 		
-
+		GUI.color = moneyLabelColor;
 		GUI.Label(new Rect (10,10, 100, 80), "$" + gameLogic.score.ToString());
+		RestoreGUIColor();
+
+		GUI.color = roundLabelColor;
+		GUI.Label(new Rect (100,10, 100, 80), "Round " + (gameLogic.round + 1).ToString());
+		RestoreGUIColor();
+
 	}
 
+	private Color moneyLabelColor = ColorWithHex(0xA0A185);
+	private Color roundLabelColor = ColorWithHex(0x3C3F39);
+	private Color disabledColor = new Color(0.0f,0.0f,0.0f, 0.2f);
 
-	private void SetDisabledColor () {
-		GUI.color = new Color(0.0f,0.0f,0.0f, 0.2f);
-	}
-
-	private void RestoreDisabledColor () {
+	private void RestoreGUIColor () {
 		GUI.color = Color.white;	
 	}
+
+
+	static Color ColorWithHex(int hex){
+	    // 0xRRGGBB
+	    float r = ((hex & 0xFF0000) >> 16)/255.0f;
+	    float g = ((hex & 0xFF00) >> 8)/255.0f;
+	    float b = (hex & 0xFF)/255.0f;
+	    return new Color(r,g,b,1.0f);
+	}
+
 
 	private int addUnitCost = 50;
 
